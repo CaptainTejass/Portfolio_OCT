f7aa671a701e12d302ea360360e5022b7b909fec
Your Name <tejasnim000@gmail.com>
2024-10-15 17:37:52 +0530

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio_OCT/',  // Replace 'your-repository' with the name of your GitHub repository
+  base: '/Portfolio_OCT/',  // Replace 'your-repository' with the name of your GitHub repository
 });
